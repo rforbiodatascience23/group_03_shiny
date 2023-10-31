be92fd9265e8a36e4e5bc41b9c4ecc6894496967
jobaov <jobao@dtu.dk>
2023-10-31 10:25:40 +0000

    Changes in module 1.

--- R/mod_DNA_Expression.R
+++ R/mod_DNA_Expression.R
@@ -4,28 +4,45 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_DNA_Expression_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    fluidRow(
+      column(8, shiny::uiOutput(ns("DNA"))),
+      column(4, shiny::numericInput(
+        inputId = ns("dna_length"),
+        value = 6000,
+        min = 3,
+        max = 100000,
+        step = 3,
+        label = "Random DNA length"
+      ),
+      shiny::actionButton(
+        inputId = ns("generate_dna"),
+        label = "Generate random DNA", style = "margin-top: 18px;"
+      ))
+    ),
+    shiny::verbatimTextOutput(outputId = ns("peptide")) |>
+      shiny::tagAppendAttributes(style = "white-space: pre-wrap;")
+
   )
 }
-    
+
 #' DNA_Expression Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_DNA_Expression_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_DNA_Expression_ui("DNA_Expression_1")
-    
+
 ## To be copied in the server
 # mod_DNA_Expression_server("DNA_Expression_1")
